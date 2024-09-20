42f3fad8873339c92df39028ec11a9ccb5c5fdf6
Meister7K <kmanfinkel17@gmail.com>
2024-09-20 16:58:29 -0600

    fix?

--- fantasy-dash/next.config.mjs
+++ fantasy-dash/next.config.mjs
@@ -1,19 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        fs: false,
-      };
-    }
-    config.experiments = {
-      ...config.experiments,
-      topLevelAwait: true,
-    };
-    return config;
-  },
 };
+
 
 export default nextConfig;
